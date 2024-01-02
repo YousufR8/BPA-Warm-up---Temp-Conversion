e67f6cfe7f9cda070fb889ac0c5e395f4f334f67
YousufR8 <Yrehman06@gmail.com>
2024-01-02 12:55:45 -0500

    Add project files.

---
+++ BPA Warm-up  - Temp Conversion/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BPA_Warm_up____Temp_Conversion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int F = 0;
+            int Celsius = (F - 32) * (5 / 9);
+            MessageBox.Show("");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int C = 0;
+            int Fahrenheit = C * (9 / 5) + 32;
+            MessageBox.Show("");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine();
+        }
+    }
+}
